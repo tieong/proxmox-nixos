12d8ac354a48b12b71bea003b9db7f25639f65f6
Thomas Ieong <th.ieong@free.fr>
2025-05-24 22:24:58 +0200

    fix: Add lxc includes fix

--- pkgs/pve-container/default.nix
+++ pkgs/pve-container/default.nix
@@ -40,6 +40,7 @@ perl538.pkgs.toPerlModule (
     # See: https://forum.proxmox.com/threads/lxc-container-creation-failing-dsa-ssh-key-generation-error.155813/
     patches = [
       ./fix-dsa-keys.patch
+      ./lxc-includes.patch
     ];
 
     postPatch = ''
