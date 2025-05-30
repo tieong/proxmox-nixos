785e5d5582c749fcd5db43fe34f389f3a0d1fae8
Thomas Ieong <th.ieong@free.fr>
2025-05-30 16:21:38 +0200

    unconfined

--- pkgs/pve-container/default.nix
+++ pkgs/pve-container/default.nix
@@ -53,7 +53,7 @@ perl538.pkgs.toPerlModule (
     # See: https://forum.proxmox.com/threads/lxc-container-creation-failing-dsa-ssh-key-generation-error.155813/
     patches = [
       ./fix-dsa-keys.patch
-      ./lxc-includes.patch
+      ./unconfined.patch
       ./fix-lxc-detach.patch
     ];
 
