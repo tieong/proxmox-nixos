525487b55f76e626369568a4d8740ddaa7518048
Thomas Ieong <th.ieong@free.fr>
2025-05-30 22:15:55 +0200

    Fixed path for unpriviliged containers

--- pkgs/pve-container/default.nix
+++ pkgs/pve-container/default.nix
@@ -99,6 +99,8 @@ perl538.pkgs.toPerlModule (
         -e "s|'/bin/umount'|'${util-linux}/bin/umount'|" \
         -e "s|@lxcfspath@|${lxcfs}|" \
         -e "s|@lxcpath@|${lxc}|" \
+        -e "s|$LXC_CONFIG_PATH/common.seccomp|${lxc}/share/lxc/config/common.seccomp|" \
+        -e "s|$cfgpath/userns.conf|$cfgpath/userns.conf.d/01-pve.conf|" \
         -e "s|lxc-console|${lxc}/bin/lxc-console|" \
         -e "s|lxc-device|${lxc}/bin/lxc-device|" \
         -e "s|lxc-attach|${lxc}/bin/lxc-attach|" \
