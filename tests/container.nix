0ce9bdc6b487f2ce292e923e3768299a5240981a
Thomas Ieong <th.ieong@free.fr>
2025-06-17 12:45:35 +0200

    Add test for container

---
+++ tests/container.nix
@@ -0,0 +1,31 @@
+{
+  name = "pve-container";
+
+  nodes.mypve = {
+    services.proxmox-ve = {
+      enable = true;
+      ipAddress = "192.168.1.1";
+
+      bridges = [ "vmbr0" ];
+    };
+  };
+
+  testScript = ''
+    machine.start()
+    machine.wait_for_unit("pveproxy.service")
+    assert "update successful" in machine.succeed("pveam update")
+    assert "debian" in machine.succeed("pveam available")
+    machine.succeed("pveam download local alpine-3.21-default_20241217_amd64.tar.xz")
+    args = (
+        "150",
+        "local:vztmpl/alpine-3.21-default_20241217_amd64.tar.xz",
+        "--hostname=alpine-test",
+        "--ostype=alpine",
+        "--unprivileged=1",
+        "--net0 name=eth0,bridge=vmbr0,ip=dhcp",
+        "--storage=local"
+    )
+    machine.succeed(f"pct create {args}")
+    machine.succeed("pct start 150")
+  '';
+}
