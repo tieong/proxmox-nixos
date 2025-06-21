484a1d20e305c7913eab5e43d9de34829016ed01
Thomas Ieong <th.ieong@free.fr>
2025-06-21 10:52:26 +0200

    Add container.nix test

--- tests/container.nix
+++ tests/container.nix
@@ -1,7 +1,43 @@
+{ pkgs, ... }:
+
+let
+  alpineTemplate = pkgs.fetchurl {
+    url = "http://download.proxmox.com/images/system/alpine-3.21-default_20241217_amd64.tar.xz";
+    hash = "sha256-plre/mIHdIgU4xWU+9xErP+L4i460ZbcKq8iy2n4HT8=";
+  };
+in
 {
   name = "pve-container";
 
   nodes.mypve = {
+    networking = {
+      useNetworkd = true;
+      firewall.enable = false;
+    };
+    #services.resolved.enable = true;
+    systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
+    systemd.network = {
+      enable = true;
+      netdevs = {
+        "vmbr0" = {
+          netdevConfig = {
+            Name = "vmbr0";
+            Kind = "bridge";
+          };
+        };
+      };
+
+    networks."10-lan-bridge" = {
+      matchConfig.Name = "vmbr0";
+      networkConfig = {
+        Address = "10.10.10.10/24";
+        Gateway = "10.10.10.254";
+        DNS = [ "8.8.8.8" ];
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
+    };
+
     services.proxmox-ve = {
       enable = true;
       ipAddress = "192.168.1.1";
@@ -26,21 +62,20 @@
   };
 
   testScript = ''
-    machine.start()
-    machine.wait_for_unit("pveproxy.service")
-    assert "update successful" in machine.succeed("pveam update")
-    assert "debian" in machine.succeed("pveam available")
-    machine.succeed("pveam download local alpine-3.21-default_20241217_amd64.tar.xz")
-    args = (
-        "150",
-        "local:vztmpl/alpine-3.21-default_20241217_amd64.tar.xz",
-        "--hostname=alpine-test",
-        "--ostype=alpine",
-        "--unprivileged=1",
-        "--net0 name=eth0,bridge=vmbr0,ip=dhcp",
-        "--storage=local"
-    )
-    machine.succeed(f"pct create {args}")
-    machine.succeed("pct start 150")
+   machine.start()
+   machine.wait_for_unit("pveproxy.service")
+   machine.succed("cp ${alpineTemplate} /var/lib/vz/template/cache/alpine-3.21-default_20241217_amd64.tar.xz")
+   machine.succeed("pveam list local")
+   args = (
+       "150",
+       "local:vztmpl/alpine-3.21-default_20241217_amd64.tar.xz",
+       "--hostname=alpine-test",
+       "--ostype=alpine",
+       "--unprivileged=1",
+       "--net0 name=eth0,bridge=vmbr0,ip=dhcp",
+       "--storage=local"
+   )
+   machine.succeed(f"pct create {' '.join(args)}")
+   machine.succeed("pct start 150 --debug")
   '';
 }
